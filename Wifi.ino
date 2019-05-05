2f800de783c6f582c9d42977ac17b24304d84688
Mervi Koiste <t6kome00@students.oamk.fi>
2019-05-05 14:31:57 +0300

    Updates

--- Wifi.ino
+++ Wifi.ino
@@ -1,18 +1,31 @@
 #include <AltSoftSerial.h>
 
-
+// TMP36 parameters
 int tempSensorPin = A0;  // Temp sensor in pin0
+
+// Lock parameters
+int highLowPin = 12; //Pin to set High and 
+int highLowPin2 = 14;
+int lock = 1;
+int lock2 = 2;
 
 bool DEBUG = true;   //show more logs
 int responseTime = 100; //communication timeout
 #define serialBaudrate 115200  // Baudrate for Arduino serial bus (PC<->Arduino)
 #define wifiSerialBaudrate 9600 // Baudrate for wifiSerial (Arduino<->ESP8266 wifi module)
 
+int time1 = 4;
+int time2 = 6;
+int time3 = 8;
+int time4 = 10;
+int time5 = 12;
+
 
 AltSoftSerial wifiSerial;      //  for ESP8266
 
 void setup()
 {
+  pinMode(highLowPin,OUTPUT);
   // Open serial communications and wait for port to open esp8266:
   Serial.begin(serialBaudrate);
   while (!Serial) {
@@ -75,16 +88,27 @@ void loop()
         sendData("\nError reading message");               //At command ERROR CODE for Failed Executing statement
     }else
     if(find(message,"HELLO")){  //receives HELLO from wifi
-        Serial.print("\\nHI!");    //arduino says HI
-        sendData("\\nHI!");
+        Serial.println("HI!");    //arduino says HI
+        sendData("HI!");
     }else if(find(message,"TEMP")){
-      Serial.print("\\nTEMP:22");    //arduino sends temperature
-      sendData("\\nTEMP:22");
-    }
+      Serial.println("TEMP:22");    //arduino sends temperature
+      sendData("TEMP:22");
+    }else
+    if(find(message,"time1")){  //receives timevalue from wifi
+      calculateTime(time1);    
+    }else if(find(message,"time2")){ //receives timevalue from wifi
+      calculateTime(time2);
+    }else if(find(message,"time3")){ //receives timevalue from wifi
+      calculateTime(time3);
+    }else if(find(message,"time4")){ //receives timevalue from wifi
+      calculateTime(time4);
+    }else if(find(message,"time5")){ //receives timevalue from wifi
+      calculateTime(time5);
+      }
     else{
       Serial.print("\nUnknown command!");
       Serial.println(message);
-      sendData("\nUnknown command!");                 //Command ERROR CODE for UNABLE TO READ
+      sendData("\nUnknown command!");                 //Command ERROR CODE for UNABLE TO READ 
     }
   }
   delay(responseTime);
@@ -229,3 +253,93 @@ String sendToUno(String command, const int timeout, boolean debug){
   }
   return response;
 }
+
+/***********************
+* Name: calculateTime
+* Description: Function calculate 80% of given time, start timer and ask temperature
+*
+************************/
+
+int calculateTime (int giventime)
+{
+ int time = giventime;
+ int partTime = 0;
+ partTime = time / 100 * 80; // partTime is integer because it is enough exact time
+ unsigned long waitTime = partTime * 60 * 60* 1000; //changing hours to msecond
+ time = (time - partTime)* 60 * 60 * 1000; //calculate remaining time 
+ delay(waitTime);
+ 
+ if (temperature()){
+    openLock(lock);
+    secondTimer();
+ }
+ else{
+    delay(time);
+    openLock(lock);
+ }
+}
+
+/***********************
+* Name: temperature
+* Description: Function read value from pin A0
+* Returns: True or False
+*************************/
+
+boolean temperature(){
+
+ //getting the voltage reading from the temperature sensor
+/* int readPin = analogRead(sensorPin);
+ float temperatureC = 0; //Readed and modified temperature 
+ 
+ // converting that reading to voltage
+ float voltage = readPin * 3.3;
+ voltage /= 1024.0; 
+ 
+ 
+ temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                        //to degrees ((voltage - 500mV) times 100)
+ delay(1000);                                     //waiting a second
+ if (temperatureC <= -20.0){
+    return (true);
+ }
+ else{
+    return (false);
+ }*/
+ return (true);
+}
+
+/************************
+* Name: openLock
+* Description: Funtion send pulse to magneticlock and open it
+* Returns: -
+*************************/
+
+int openLock(int lock){
+
+  if (lock == 1){
+    digitalWrite(highLowPin, HIGH); // sets power on to relay
+    delay(2000);                      // waits for two second
+    digitalWrite(highLowPin, LOW);  // sets power off to relay
+    delay(2000);
+  }else if (lock == 2){
+    digitalWrite(highLowPin2, HIGH); // sets power on to relay
+    delay(2000);                      // waits for two second
+    digitalWrite(highLowPin2, LOW);  // sets power off to relay
+    delay(2000);
+    }
+ 
+}
+ 
+ /***********************
+ * Name: secondTimer
+ * Description: Function count time for second lock
+ * Returns: _
+ ************************/
+ 
+ void secondTimer() {
+ 
+    unsigned long waitTime = 2UL * 60 * 60* 1000; //changing hours to second
+ 
+    delay(waitTime);
+    openLock(lock2);
+ }
